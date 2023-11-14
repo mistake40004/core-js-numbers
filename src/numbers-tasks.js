ed6eeddaa0712c9ae0715b216bb7af78b404acf1
Alina <alinarafaelevna@mail.ru>
2023-11-14 20:50:49 +0300

    fix: numbers 34 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -576,8 +576,8 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomInteger(/* min, max */) {
+  throw new Error('Not implemented');
 }
 
 /**
